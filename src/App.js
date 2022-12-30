dd46294d63af5b3352f946eb25721c1a2b71a2bb
mmulahvictor <mmulahvictor@gmail.com>
2022-12-30 13:10:22 +0300

    style the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Skills from './components/skills/Skills';
 import Contact from './components/contact/Contact';
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Style.css'
+import './navbar.css'
 
 function Navbar () {
     return (
         <nav className='nav'>
-            <h1 className='logo'>Victor<span className='my_name'>Mmulah</span></h1>
+            <NavLink to="/" className='logo'>Victor<span className='my_name'>Mmulah</span></NavLink>
             <NavLink to="/" className='link'>Home</NavLink>
             <NavLink to="/skills" className='link'>Skills</NavLink>
             <NavLink to="/blogs" className='link'>Blogs</NavLink>
