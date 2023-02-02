a8f44b717755b251815831453043a1a61ad9b66b
mmulahvictor <mmulahvictor@gmail.com>
2023-02-02 12:54:21 +0300

    recreating the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,27 +1,104 @@
-import React from 'react';
-import Footer from '../footer/Footer';
+import React, { useState, useEffect } from 'react';
+// import Footer from '../footer/Footer';
 import './home.css';
 
 function Home () {
-  return (
-    <div className='home'>
-      <div className='header'>
-        <img src="https://raw.githubusercontent.com/mmulahvictor/myPortfolio/main/src/assets/IMG-20220630-WA0019.jpg" alt="Victor's avatar" />
-        <h1>Victor Mmulah</h1>
-        <h2>Software Engineer/IT Officer</h2>
-      </div>
-      <div className='home-msg'>
-        <img src="https://raw.githubusercontent.com/mmulahvictor/myPortfolio/main/src/assets/IMG-20220630-WA0019.jpg" alt="Victor" />
-        <div className='body'>
-          <h4>Discover</h4>
-          <h2>About Me</h2>
-          <p>
-            I am a software engineer based in Kakamega, Kenya and I am very passionate and dedicated to my work. With 3 years experience I am a professional full stack developer. I have acquired the skills necessary to build great and premium websites and softwares.
-          </p>
 
+  const [ showScroll, setShowScroll ] = useState( false );
+  const [ isOpen, setIsOpen ] = useState( false );
+
+  const toggleNav = () => {
+    setIsOpen( !isOpen );
+  };
+
+  useEffect( () => {
+    window.addEventListener( "scroll", () => {
+      if ( window.pageYOffset > 100 )
+      {
+        setShowScroll( true );
+      } else
+      {
+        setShowScroll( false );
+      }
+    } );
+  }, [] );
+
+  const scrollTop = () => {
+    window.scrollTo( { top: 0, behavior: "smooth" } );
+  };
+
+  return (
+    <div className="homepage">
+      <header className="header">
+        <button className="toggle-nav" onClick={ toggleNav }>
+          &#9776;
+        </button>
+        { isOpen && (
+          <nav>
+            <a href="#about-me">About Me</a>
+            <a href="#skills">Skills</a>
+            <a href="#experience">Experience</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        ) }
+        <h1 className="title">Victor Mmulah</h1>
+        <h2 className="subtitle">Full Stack Software Developer</h2>
+      </header>
+      <section className="about-me">
+        <h3 className="section-title">About Me</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </section>
+      <section className="skills">
+        <h3 className="section-title">Skills</h3>
+        <ul className="skill-list">
+          <li>JavaScript</li>
+          <li>Ruby</li>
+          <li>ReactJs</li>
+          <li>Rails</li>
+        </ul>
+      </section>
+      <section className="work-experience">
+        <h3 className="section-title">Work Experience</h3>
+        <div className="experience-item">
+          <h4 className="position">IT Support at KALRO</h4>
+          <p className="duration">Duration: [Duration]</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
-      </div>
-      <Footer />
+        <div className="experience-item">
+          <h4 className="position">Software Developer at INNODEMS</h4>
+          <p className="duration">Duration: [Duration]</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="experience-item">
+          <h4 className="position">ICT Officer at Edutab Africa</h4>
+          <p className="duration">Duration: [Duration]</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </section>
+      <section className="contact">
+        <h3 className="section-title">Get In Touch</h3>
+        <p>Email: mmulahvictor@gmail.com</p>
+        <p>LinkedIn: https://www.linkedin.com/in/victor-mmulah-51666819b</p>
+      </section>
+      { showScroll && (
+        <button className="scroll-top" onClick={ scrollTop }>
+          Scroll to Top
+        </button>
+      ) }
     </div>
   );
 }
